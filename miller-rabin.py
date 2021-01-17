430c1f05b187bcbc3d12a22a06109ac8d424eea7
Alan Tan <alanwuha91@gmail.com>
2021-01-17 17:03:07 +0800

    feat: add Miller-Rabin algorithm steps as comments

--- miller-rabin.py
+++ miller-rabin.py
@@ -1,4 +1,12 @@
 class MillerRabin:
+    """ The Miller Rabin algorithm.
+
+    The algorithm.
+    1) Generate a prime candidate.
+    2) Test if the generated number is prime.
+    3) If the number is not prime, restart from beginning.
+
+    """
 
     def __init__(self):
         print('Hello world, I am a MillerRabin object instance!')
